8930bc8c6d7f9b4deb6d9839f3709649b76c52cf
Unknown <martinhaukali@gmail.com>
2018-01-29 09:20:40 +0100

    RX Anomaly testing
    RX anomaly detectors can be tested using this script. The RX anomaly section starts at line 41.

--- functions/hyperDemo_detectors.m
+++ functions/hyperDemo_detectors.m
@@ -2,34 +2,35 @@ function hyperDemo_detectors
 % HYPERDEMO_DETECTORS Demonstrates target detector algorithms
 clear; clc; dbstop if error; close all;
 %--------------------------------------------------------------------------
-% Parameters
-resultsDir = 'results\\';
-dataDir = 'data\\AVIRIS\\';
+%% Parameters
+resultsDir ='E:\One Drive\OneDrive for Business\NTNU\Master\MATLAB_Hyperspectral_toolbox\results';
+dataDir = 'E:\One Drive\OneDrive for Business\NTNU\Master\MATLAB_DEMO_hyperspectral\f970619t01p02r02c';
 %--------------------------------------------------------------------------
 
 mkdir(resultsDir);
 
-% Read part of AVIRIS data file that we will further process
-M = hyperReadAvirisRfl(sprintf('%s\\f970620t01p02_r03_sc02.a.rfl', dataDir), [1 100], [1 614], [1 224]);
+%% Read part of AVIRIS data file that we will further process
+M = hyperReadAvirisRfl(sprintf('%s\\f970619t01p02_r02_sc02.a.rfl', dataDir), [1 100], [1 614], [1 224]);
 M = hyperNormalize(M);
+%M_test = imread('E:\One Drive\OneDrive for Business\NTNU\Master\MATLAB_DEMO_hyperspectral\220_band_aviris_june12\aviris_hyperspectral_data\19920612_AVIRIS_IndianPine_EW-line_R.tif');
+%M_test = hyperNormalize(M_test);
+%% Read AVIRIS .spc file
+lambdasNm = hyperReadAvirisSpc(sprintf('%s\\f970619t01p02_r02.a.spc', dataDir));
 
-% Read AVIRIS .spc file
-lambdasNm = hyperReadAvirisSpc(sprintf('%s\\f970620t01p02_r03.a.spc', dataDir));
-
-% Isomorph
+%% Isomorph
 [h, w, p] = size(M);
 M = hyperConvert2d(M);
 
-% Resample AVIRIS image.
+%% Resample AVIRIS image.
 desiredLambdasNm = 400:(2400-400)/(224-1):2400;
 M = hyperResample(M, lambdasNm, desiredLambdasNm);
 
-% Remove low SNR bands.
+%% Remove low SNR bands.
 goodBands = [10:100 116:150 180:216];
 M = M(goodBands, :);
 p = length(goodBands);
 
-% Demonstrate difference spectral similarity measurements
+%% Demonstrate difference spectral similarity measurements
 M = hyperConvert3d(M, h, w, p);
 target = squeeze(M(11, 77, :));
 figure; plot(desiredLambdasNm(goodBands), target); grid on;
@@ -37,42 +38,52 @@ figure; plot(desiredLambdasNm(goodBands), target); grid on;
 
 M = hyperConvert2d(M);
   
-% RX Anomly Detector
-r = hyperRxDetector(M);
-r = hyperConvert3d(r.', h, w, 1);
-figure; imagesc(r); title('RX Detector Results'); axis image;
-    colorbar;
-hyperSaveFigure(gcf, sprintf('%s\\rx detector.png', resultsDir));
+%% RX Anomly Detector
+%r = hyperRxDetector(M);
+%r = hyperRxDetectorCor(M);
+K=25;
+%M_test_to = cast(M,'double');
+%r = hyperLRxDetectorCorr(M,K,0);
+%for tresh= 271: 1 :289
+tresh = 275; 
+[r, anomalies_detected] = hyperACAD(M,tresh);
 
-% Constrained Energy Minimization (CEM)
+r = hyperConvert3d(r.', h, w, 1);
+figure; imagesc(r); title(['RX Detector Results, tresh =' num2str(tresh) '.'] ); axis image;
+    colorbar;
+hyperSaveFigure(gcf, sprintf(['%s\\rx detector' num2str(tresh) '.png' ], resultsDir));%
+
+%end
+
+%% Constrained Energy Minimization (CEM)
 r = hyperCem(M, target);
 r = hyperConvert3d(r, h, w, 1);
 figure; imagesc(abs(r)); title('CEM Detector Results'); axis image;
     colorbar;    
 hyperSaveFigure(gcf, sprintf('%s\\cem detector.png', resultsDir));
 
-% Adaptive Cosine Estimator (ACE)
+%% Adaptive Cosine Estimator (ACE)
 r = hyperAce(M, target);
 r = hyperConvert3d(r, h, w, 1);
 figure; imagesc(r); title('ACE Detector Results'); axis image;
     colorbar;
 hyperSaveFigure(gcf, sprintf('%s\\ace detector.png', resultsDir));    
 
-% Signed Adaptive Cosine Estimator (S-ACE)
+%% Signed Adaptive Cosine Estimator (S-ACE)
 r = hyperSignedAce(M, target);
 r = hyperConvert3d(r, h, w, 1);
 figure; imagesc(r); title('Signed ACE Detector Results'); axis image;
     colorbar;
 hyperSaveFigure(gcf, sprintf('%s\\signed ace detector.png', resultsDir));  
 
-% Matched Filter
+%% Matched Filter
 r = hyperMatchedFilter(M, target);
 r = hyperConvert3d(r, h, w, 1);
 figure; imagesc(r); title('MF Detector Results'); axis image;
     colorbar;
 hyperSaveFigure(gcf, sprintf('%s\\mf detector.png', resultsDir)); 
 
-% Generalized Likehood Ratio Test (GLRT) detector
+%% Generalized Likehood Ratio Test (GLRT) detector
 r = hyperGlrt(M, target);
 r = hyperConvert3d(r, h, w, 1);
 figure; imagesc(r); title('GLRT Detector Results'); axis image;
@@ -80,17 +91,17 @@ figure; imagesc(r); title('GLRT Detector Results'); axis image;
 hyperSaveFigure(gcf, sprintf('%s\\cem detector.png', resultsDir));
 
 
-% Estimate background endmembers
+%% Estimate background endmembers
 U = hyperAtgp(M, 5);
 
-% Hybrid Unstructured Detector (HUD)
+%% Hybrid Unstructured Detector (HUD)
 r = hyperHud(M, U, target);
 r = hyperConvert3d(r, h, w, 1);
 figure; imagesc(abs(r)); title('HUD Detector Results'); axis image;
     colorbar;
 hyperSaveFigure(gcf, sprintf('%s\\hud detector.png', resultsDir));
    
-% Adaptive Matched Subspace Detector (AMSD)
+%% Adaptive Matched Subspace Detector (AMSD)
 r = hyperAmsd(M, U, target);
 r = hyperConvert3d(r, h, w, 1);
 figure; imagesc(abs(r)); title('AMSD Detector Results'); axis image;
@@ -98,7 +109,7 @@ figure; imagesc(abs(r)); title('AMSD Detector Results'); axis image;
 hyperSaveFigure(gcf, sprintf('%s\\amsd detector.png', resultsDir));    
 figure; mesh(r); title('AMSD Detector Results');
 
-% Orthogonal Subspace Projection (OSP)
+%% Orthogonal Subspace Projection (OSP)
 r = hyperOsp(M, U, target);
 r = hyperConvert3d(r, h, w, 1);
 figure; imagesc(abs(r)); title('OSP Detector Results'); axis image;
